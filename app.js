d34c1d37c25b4db08c0e44c688bf2c75d85078bf
Anochiwa Alfred <anochiwaalfred@gmail.com>
2024-02-26 10:22:29 +0100

    Touches

--- app.js
+++ app.js
@@ -8,42 +8,7 @@ const completeHeading = document.querySelector("#completedTasksHeading")
 incompleteHeading.style.display = "none"
 completeHeading.style.display = "none"
 
-var allList = document.querySelector("#allList")
 
-
-document.addEventListener("DOMContentLoaded", function() {
-  document.querySelectorAll('.mark-completed-btn').forEach(button => {
-    button.addEventListener('click', markCompletedHandler);
-  });
-
-  document.querySelectorAll('.edit-btn').forEach(button => {
-    button.addEventListener('click', editHandler);
-  });
-
-  document.querySelectorAll('.delete-btn').forEach(button => {
-    button.addEventListener('click', deleteHandler);
-  });
-
-  document.querySelectorAll('.edit-toggle-button').forEach(button => {
-    button.addEventListener('click', preEditHandler);
-  });
-
-  document.querySelectorAll('.mark-incomplete-btn').forEach(button => {
-    button.addEventListener('click', markIncompleteHandler);
-  });
-
-  document.querySelectorAll('.delete-btn').forEach(button => {
-    button.addEventListener('click', deleteHandler);
-  });
-
-  function preEditHandler(event) {
-    const itemId = event.target.getAttribute('data-item-id');
-    const taskText = document.getElementById(`text-${itemId}`); 
-    const todoContentInputEdit = document.getElementById(`todoContentEdit-${itemId}`);
-    const textttt = taskText.innerHTML
-    todoContentInputEdit.value = textttt;
-  }
-})
 
 
 
@@ -78,28 +43,29 @@ function displayList() {
 
 
                     <button type="button" class="btn btn-sm me-2 ms-2 btn-warning edit-toggle-button" data-bs-toggle="modal" data-bs-target="#editTaskModal-${incompleteToDoList.length}"  data-item-id="${incompleteToDoList.length}">Edit</button>
+                    
                     <!-- Modal -->
                     <div class="modal fade" id="editTaskModal-${incompleteToDoList.length}" tabindex="-1" aria-labelledby="editTaskModalLabel" aria-hidden="true">
-                    <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
-                        <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title text-dark" id="editTaskModalLabel">Edit Task</h5>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
-                        <div class="modal-body text-dark">
-                            <form id="todoFormEdit-${incompleteToDoList.length}" class="form-inline">
-                                <input type="text" id="todoContentEdit-${incompleteToDoList.length}" class="form-control mb-2 mr-sm-2" placeholder="Type here...">
-                            </form>
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-sm btn-secondary" data-bs-dismiss="modal">Close</button>
-                            <button type="button" class="btn btn-sm btn-primary animate__animated animate__swing edit-btn"  id="submitTodoEdit-${incompleteToDoList.length}" data-item-id="${incompleteToDoList.length}" onclick="editHandler('${incompleteToDoList.length}')">Save changes</button>
-                        </div>
-                        </div>
-                    </div>
+                      <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+                          <div class="modal-content">
+                          <div class="modal-header">
+                              <h5 class="modal-title text-dark" id="editTaskModalLabel">Edit Task</h5>
+                              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                          </div>
+                          <div class="modal-body text-dark">
+                              <form id="todoFormEdit-${incompleteToDoList.length}" class="form-inline">
+                                  <input type="text" id="todoContentEdit-${incompleteToDoList.length}" class="form-control mb-2 mr-sm-2" placeholder="Type here...">
+                              </form>
+                          </div>
+                          <div class="modal-footer">
+                              <button type="button" class="btn btn-sm btn-secondary" data-bs-dismiss="modal">Close</button>
+                              <button type="button" class="btn btn-sm btn-primary animate__animated animate__swing edit-btn"  id="submitTodoEdit-${incompleteToDoList.length}" data-item-id="${incompleteToDoList.length}" onclick="editHandler('${incompleteToDoList.length}')">Save changes</button>
+                          </div>
+                          </div>
+                      </div>
                     </div>
 
-                    <button type="submit" class="btn btn-sm delete-btn btn-danger"  data-item-id="${incompleteToDoList.length}" onclick="deleteHandler('list-${incompleteToDoList.length}')">Delete</button>
+                    <button type="submit" class="btn btn-sm delete-btn btn-danger"-  data-item-id="${incompleteToDoList.length}" onclick="deleteHandler('list-${incompleteToDoList.length}')">Delete</button>
             </div>
         `;
 
@@ -133,7 +99,6 @@ function displayList() {
 function markCompletedHandler(id) {
   listItem = document.getElementById(id)
   let itemText = listItem.querySelector("span").innerText;
-  console.log(itemText);
 
   if (!completeToDoList.includes(itemText)) {
     completeToDoList.push(itemText);
@@ -151,7 +116,6 @@ function markCompletedHandler(id) {
 function markIncompletedHandler(id) {
   listItem = document.getElementById(id)
   let itemText = listItem.querySelector("span").innerText;
-  console.log(itemText);
 
   if (!incompleteToDoList.includes(itemText)) {
     incompleteToDoList.push(itemText);
